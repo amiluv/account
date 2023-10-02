e5c02e14c0000ac4bfb1fae0de4b69ef2f3ad460
amiluv <augustineug@gmail.com>
2023-10-02 13:43:03 +0100

    updated

--- appcode.js
+++ appcode.js
@@ -12,16 +12,22 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 //referencing the database
-var signupFormDB = firebase.database().ref("singupForm");
-  
+const database = firebase.database();
+//const signupFormDB = firebase.database().ref("signupForm");
+const signupFormDB = database.ref("users");
+
 //get IDs
 const signupForm = document.getElementById("signupForm");
 const loginForm = document.getElementById("loginForm");
 const showSignup = document.getElementById("showSignup");
 const showLogin = document.getElementById("showLogin"); 
+const errorEmail = document.getElementById("error-email");
+const errorPhone = document.getElementById("error-phone");
+const phoneNumberInput = document.getElementById("signupPhoneNumber");
+const emailInput = document.getElementById("signupEmail");
+
 
 //toggle between signup and login forms
-
 showSignup.addEventListener("click", function() {
 signupForm.classList.remove("hidden");
 loginForm.classList.add("hidden");
@@ -34,8 +40,23 @@ signupForm.classList.add("hidden");
 document.getElementById("loginEmail").focus();
 });
 
-//listen for signup form submit
+//check for valid phone number
+phoneNumberInput.addEventListener("input", function(){
+  const inputValue = phoneNumberInput.value;
+  const numericValue = inputValue.replace(/\D/g, "");
+  if (numericValue.length === 11){
+     errorPhone.textContent = "";
+  } else if(inputValue == numericValue){
+    errorPhone.textContent = '';
+  }
+   else {
+    errorPhone.textContent = "Enter 11 digit numbers only"
+  }
+  phoneNumberInput.value = numericValue;
+});
 
+/
+//listen for signup form submit
 signupForm.addEventListener("submit", function(e) {
 e.preventDefault();
 const email = document.getElementById("signupEmail").value;
@@ -43,44 +64,62 @@ const password = document.getElementById("signupPassword").value;
 const confirmPassword = document.getElementById("signupConfirmPassword").value;
 const phoneNumber = document.getElementById("signupPhoneNumber").value;
 
-if (password === confirmPassword) {
-  //localStorage.setItem(email, JSON.stringify({ password, phoneNumber }));
-  saveDetails(email, password, phoneNumber);    
-           
-  alert("Signup successful!");
-
+if (password === confirmPassword && email !== "") {
+  saveDetails(email, password, phoneNumber); 
+  document.querySelector(".alert").style.display = "block";
+  setTimeout(function() {
+    document.querySelector(".alert").style.display = "none";
+  },3000);
   signupForm.reset();
-}
-else {
-  alert("Passwords do not match.");
-}
-//console.log(email, password, phoneNumber);
+  }else {
+    alert("Passwords do not match or email is empty.");
+  }
 });
 
 //listen for login form submit
-
 loginForm.addEventListener("submit", function(e) {
-e.preventDefault();
-const email = document.getElementById("loginEmail").value;
-const password = document.getElementById("loginPassword").value;
-const storedData = JSON.parse(localStorage.getItem(email));
+  e.preventDefault();
+  const email = document.getElementById("loginEmail").value;
+  const password = document.getElementById("loginPassword").value;
 
-if (storedData && storedData.password === password) {
-  alert("Login successful!");
-  loginForm.reset();
-} 
-else {
-  alert("Invalid email or password.");
-}
+  // Reference to the users' data location in Firebase 
+  const usersRef = firebase.database().ref("users");
+  usersRef.orderByChild("email").equalTo(email).once("value")
+    .then(snapshot => {
+      if (snapshot.exists()) {
+        // User with the provided email exists in the database
+        snapshot.forEach(childSnapshot => {
+          const userData = childSnapshot.val();
+          if (userData.password === password) {
+            document.querySelector(".alert").style.display = "block";
+            setTimeout(function() {
+              document.querySelector(".alert").style.display = "none";
+            },2000);
+            setTimeout(function(){
+              window.location.href = "index.html";
+            },1000);
+            
+          } else {
+            // Password does not match
+            alert("Invalid email or password.");
+          }
+        });
+      } else {
+        // User with the provided email does not exist
+        alert("Invalid email or password.");
+      }
+    })
+    .catch(error => {
+      console.error("Error reading data:", error);
+      alert("An error occurred while logging in.");
+    });
 });
 
-//save account details to data base
 function saveDetails(email, password, phoneNumber){
-var newAccount = signupFormDB.push();
-newAccount.set({
-  email: email,
-  password: password,
-  phoneNumber: phoneNumber
-});
+  const newAccount = signupFormDB.push();
+  newAccount.set({
+    email: email,
+    password: password,
+    phoneNumber: phoneNumber
+  });
 };
-
